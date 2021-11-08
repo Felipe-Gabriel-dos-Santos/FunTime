bba19ff6ee4fa8bef58cb49e3e35d869b42a3fc4
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2021-11-08 13:35:53 -0300

    removed the route parameter

--- src/screens/TelaInicial/index.js
+++ src/screens/TelaInicial/index.js
@@ -5,12 +5,12 @@ import Background from '../../components/Background';
 
 import { styles } from './style';
 
-export default function TelaInicial({ route }){
+export default function TelaInicial(){
 
 	return (
 		<Background>
 			<View style={styles.view}>
-				<Text>{route.params.Id_do_Usuário}</Text>
+				
 			</View>
 		</Background>
 	);
