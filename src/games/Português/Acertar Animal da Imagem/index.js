884e4a082f5c82d14c24120bc5fc8d9806dcaa20
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2021-11-30 06:45:24 -0300

    Improvement of the Registration / Login error system and beginning of Portuguese game development

---
+++ src/games/Português/Acertar Animal da Imagem/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Cachorro from './Imagens/cachorro.jpg';
+import Coelho from './Imagens/coelho.png';
+import Crocodilo from './Imagens/crocodilo.jpg';
+import Gato from './Imagens/gato.png';
+import Girafa from './Imagens/girafa.jpg';
+import Hipopótamo from './Imagens/hipopotamo.jpg';
+import Leão from './Imagens/leão.jpg';
+import Papagaio from './Imagens/papagaio.jpg';
+import Pinguim from './Imagens/pinguim.png';
+import Porco from './Imagens/porco.jpg';
+import Rato from './Imagens/rato.png';
+import Sapo from './Imagens/sapo.png';
+import Urso from './Imagens/urso.jpg';
+import Vaca from './Imagens/vaca.png';
+
+export default function jogoPortuguês(){
+
+	var ListaAnimais = [
+		{
+			Nome: 'Cachorro',
+			Imagem: Cachorro
+		},
+		{
+			Nome: 'Coelho',
+			Imagem: Coelho
+		},
+		{
+			Nome: 'Crocodilo',
+			Imagem: Crocodilo
+		},
+		{
+			Nome: 'Gato',
+			Imagem: Gato
+		},
+		{
+			Nome: 'Girafa',
+			Imagem: Girafa
+		},
+		{
+			Nome: 'Hipopótamo',
+			Imagem: Hipopótamo
+		},
+		{
+			Nome: 'Leão',
+			Imagem: Leão
+		},
+		{
+			Nome: 'Papagaio',
+			Imagem: Papagaio
+		},
+		{
+			Nome: 'Pinguim',
+			Imagem: Pinguim
+		},
+		{
+			Nome: 'Porco',
+			Imagem: Porco
+		},
+		{
+			Nome: 'Rato',
+			Imagem: Rato
+		},
+		{
+			Nome: 'Sapo',
+			Imagem: Sapo
+		},
+		{
+			Nome: 'Urso',
+			Imagem: Urso
+		},
+		{
+			Nome: 'Vaca',
+			Imagem: Vaca
+		}
+		
+	];
+
+	return (
+		<View>
+            
+		</View>
+
+	);
+}
